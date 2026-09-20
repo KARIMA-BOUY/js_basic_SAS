c4c88bb7132c669f3f9e1b1daf95d8870f32b921
KARIMA-BOUY <kbou9695@gmail.com>
2026-09-20 22:47:28 +0200

    Ajout challange12

---
+++ Day3/arrays/challange12.js
@@ -0,0 +1,13 @@
+// Challenge 12 : Afficher les Éléments Paire
+// Écrivez un programme JavaScript qui affiche uniquement les éléments pairs d'un tableau d'entiers.
+//  Le programme doit demander le nombre d'éléments et les éléments du tableau.
+
+const prompt = require("prompt-sync")();
+const nbr = Number(prompt("entrer les nbr d element :"));
+let tab = [];
+for (let i = 0; i < nbr; i++) {
+    const valaur = Number(prompt("entrer les valeur:"));
+    if (valaur % 2 == 0) {
+        tab.push(valaur);
+    }
+} console.log(tab);
