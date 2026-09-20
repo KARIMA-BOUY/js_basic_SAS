cca4baa7b3ebed0bd33530f25f43b273d5ee3a9a
KARIMA-BOUY <kbou9695@gmail.com>
2026-09-20 16:33:58 +0200

    Ajout challange1

---
+++ Day3/arrays/challange1.js
@@ -0,0 +1,9 @@
+// Challenge 1 : Initialisation et Affichage
+// Écrivez un programme JavaScript qui initialise un tableau d'entiers avec des valeurs données et affiche ces valeurs. 
+// Par exemple,
+// vous pouvez initialiser un tableau avec les valeurs [1, 2, 3, 4, 5] et afficher chaque valeur sur une nouvelle ligne.
+
+const tab = [1, 2, 3, 4, 5];
+for (let i = 1; i < tab.length; i++) {
+    console.log(i);
+}
