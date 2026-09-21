e25ced071a18e47e1a3ce6e6aa52055256dbdf2c
KARIMA-BOUY <kbou9695@gmail.com>
2026-09-21 18:52:04 +0200

    challange1

---
+++ Day5/functions/challange1.js
@@ -0,0 +1,11 @@
+// Challenge 1 : Fonction de Somme
+// Écrivez une fonction JavaScript qui prend deux nombres en paramètres et retourne leur somme.
+//  Utilisez cette fonction pour afficher la somme de deux nombres.
+
+
+
+function somme(nbr1,nbr2){
+   return nbr1+nbr2;
+}
+let result=somme(3,6);
+console.log(result);
