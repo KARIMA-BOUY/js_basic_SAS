de9a4c3e8394f7fcb713d9ed51b57521e6f7c0d7
KARIMA-BOUY <kbou9695@gmail.com>
2026-09-21 11:49:51 +0200

    challange1

---
+++ Day4/les-chaines/challange1.js
@@ -0,0 +1,6 @@
+// Challenge 1 : Affichage d'une Chaîne"
+// Écrivez un programme JavaScript qui lit une chaîne de caractères entrée par l'utilisateur et l'affiche à l'écran.
+
+const prompt=require("prompt-sync")();
+const text=prompt("entrer une chaine caractere ");
+console.log("vous etes ecris c est "+" "+text);
