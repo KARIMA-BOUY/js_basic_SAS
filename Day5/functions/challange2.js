3bfb84acc13d66c2d07b335b38cf4d778455fa7b
KARIMA-BOUY <kbou9695@gmail.com>
2026-09-21 19:06:17 +0200

    challange

---
+++ Day5/functions/challange2.js
@@ -0,0 +1,11 @@
+// Challenge 2 : Fonction de Multiplication
+// Écrivez une fonction JavaScript qui prend deux nombres en paramètres et retourne leur produit.
+// Utilisez cette fonction pour afficher le produit de deux nombres.
+
+function produit(a,b){
+    return a*b;
+}let result=produit(2,3);
+console.log(result);
+
+
+
