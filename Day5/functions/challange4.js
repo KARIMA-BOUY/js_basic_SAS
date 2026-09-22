c896e8f894e23ded84d594e30a2cc94b0b218ed3
KARIMA-BOUY <kbou9695@gmail.com>
2026-09-22 11:02:43 +0200

    challange

---
+++ Day5/functions/challange4.js
@@ -0,0 +1,18 @@
+// Challenge 3 : Fonction de Maximum
+// Écrivez une fonction JavaScript qui prend deux nombres en paramètres et retourne le plus grand des deux.
+//  Utilisez cette fonction pour afficher le maximum entre deux nombres.
+
+
+function Maximum(a,b){
+   if(a>b){
+    return a;
+   }else{
+    return b; 
+   }
+  
+} const result=Maximum(2,5);
+console.log(result);
+
+
+
+
