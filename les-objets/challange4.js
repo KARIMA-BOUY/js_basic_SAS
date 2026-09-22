8f69df9a0610a414aae78220123338b2806bcdc3
KARIMA-BOUY <kbou9695@gmail.com>
2026-09-22 11:54:17 +0200

    challange

---
+++ les-objets/challange4.js
@@ -0,0 +1,11 @@
+// Challenge 4 : Modification des Propriétés d'un Objet
+// Écrivez un programme JavaScript qui crée un objet pour représenter un point dans un plan avec les propriétés x et y. 
+// Modifiez directement les valeurs de ses propriétés, puis affichez les valeurs du point.
+
+const points={
+    x:10,
+    y:20,
+}
+points.x=30;
+points.y=40;
+console.log(points);
