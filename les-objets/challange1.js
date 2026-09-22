cf8768a24bc886aeb399012a85e82f00fc919f95
KARIMA-BOUY <kbou9695@gmail.com>
2026-09-22 11:35:12 +0200

    challange

---
+++ les-objets/challange1.js
@@ -0,0 +1,15 @@
+// Challenge 1 : Création et Utilisation d'un Objet
+// Écrivez un programme JavaScript qui crée un objet pour représenter une personne avec les propriétés nom,
+//  prenom et age. Attribuez des valeurs aux propriétés et affichez-les.
+
+const personne={
+    nom:"karima",
+    prenom:"bouy",
+    age:22,
+    ville:"safi",
+
+};
+console.log(personne.nom);
+console.log(personne.prenom);
+console.log(personne.age);
+console.log(personne.ville);
